43239da8741601b3305b69d7c592cc88646aff84
slapaay <slapelachie@gmail.com>
2020-05-11 14:11:08 +1000

    center

--- config.def.h
+++ config.def.h
@@ -28,11 +28,11 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class				instance    title	tags mask	isfloating	monitor */
-	{ "Gimp",				NULL,		NULL,	0,			1,  		-1 },
-	{ "Firefox",			NULL,		NULL,	1 << 8,		0,			-1 },
-	{ "Sxiv",				NULL,		NULL, 	0,			1,			-1 },
-	{ "gnome-calculator", 	NULL,		NULL,	0,			1,			-1 }
+	/* class				instance    title	tags mask	iscenter	isfloating	monitor */
+	{ "Gimp",				NULL,		NULL,	0,			0,			1,  		-1 },
+	{ "Firefox",			NULL,		NULL,	1 << 8,		0,			0,			-1 },
+	{ "Sxiv",				NULL,		NULL, 	0,			1,			1,			-1 },
+	{ "gnome-calculator", 	NULL,		NULL,	0,			1,			1,			-1 }
 };
 
 /* layout(s) */
